909ecee89fbc3d2ccded9dfb3ddc36bd5b7119c6
João Vitor Bonadiman <j.bonadiman@live.com>
2021-11-12 15:55:52 -0300

    Makes essential packages a separate file;

--- .dotfiles_ubuntu/essential_packages.sh
+++ .dotfiles_ubuntu/essential_packages.sh
@@ -7,9 +7,6 @@ sudo update-locale
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then git clone https://github.com/VundleVim/Vundle    .vim.git ~/.vim/bundle/Vundle.vim;fi
 
-sudo apt-get install httpie -y
-sudo apt-get install bat -y
-sudo apt-get install firefox -y
+xargs -a Essentials.pckg  sudo apt-get install -y
 
-sudo apt-get install clang -y
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
