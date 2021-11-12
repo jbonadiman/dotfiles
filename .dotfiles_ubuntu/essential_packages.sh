cde443771c4a81b138269cbaba23e06384c30ca2
João Vitor Bonadiman <j.bonadiman@live.com>
2021-11-12 16:56:11 -0300

    Adds exa installation and icons;

--- .dotfiles_ubuntu/essential_packages.sh
+++ .dotfiles_ubuntu/essential_packages.sh
@@ -7,6 +7,9 @@ sudo update-locale
 
 if [ ! -d ~/.vim/bundle/Vundle.vim ]; then git clone https://github.com/VundleVim/Vundle    .vim.git ~/.vim/bundle/Vundle.vim;fi
 
+sudo add-apt-repository ppa:spvkgn/exa -y
+sudo apt-get update
+
 xargs -a Essentials.pckg  sudo apt-get install -y
 
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
