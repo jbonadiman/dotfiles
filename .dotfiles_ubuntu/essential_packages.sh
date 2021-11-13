41e406a5368969a843831c789fb57fa366b96bf1
João Vitor Bonadiman <j.bonadiman@live.com>
2021-11-13 16:19:20 -0300

    Sets TODO messages to turn the script idempotent;

--- .dotfiles_ubuntu/essential_packages.sh
+++ .dotfiles_ubuntu/essential_packages.sh
@@ -1,15 +1,25 @@
 #!/usr/bin/env bash
 
-echo Installing essential packages...
-
-sudo locale-gen pt_BR pt_BR.UTF-8
-sudo update-locale
-
-if [ ! -d ~/.vim/bundle/Vundle.vim ]; then git clone https://github.com/VundleVim/Vundle    .vim.git ~/.vim/bundle/Vundle.vim;fi
+echo "Installing essential packages and settings..."
 
 sudo add-apt-repository ppa:spvkgn/exa -y
 sudo apt-get update
 
-xargs -a Essentials.pckg  sudo apt-get install -y
+xargs -a Essentials.pckg sudo apt-get install -y
 
+# TODO: if $SHELL not zsh...
+chsh -s /bin/zsh
+
+# TODO: if rustup not installed...
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
+# TODO: if locale not set...
+echo "Setting locale..."
+sudo locale-gen pt_BR pt_BR.UTF-8
+sudo update-locale
+
+if [ ! -d ~/.vim/bundle/Vundle.vim ]; then
+  echo "Installing Vundle..."
+  git clone "https://github.com/VundleVim/Vundle.vim.git" ~/.vim/bundle/Vundle.vim
+fi
+
