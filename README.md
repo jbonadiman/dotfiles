408a35c5eaf1a01293379d3eda417b3e5634ea3b
João Vitor Bonadiman <j.bonadiman@live.com>
2021-11-14 14:20:32 -0300

    Adds checks to see if packages are not installed;

--- README.md
+++ README.md
@@ -2,8 +2,9 @@ Dotfiles
 
 TODO:
 
-- bat is really slow right now with the -l option. I'm using -1 option for now, but I should change it back in bat version 0.11.0 (this improvement is marked for that version miletone).
+- exa is really slow right now with the -l option. I'm using -1 option for now, but I should change it back in exa version 0.11.0 (this improvement is marked for that version miletone).
 - Fix exes execution through WSL, so I can setup the winget to install MS Store Apps.
 - Install required fonts.
 - Set locale only if not previously set.
 - Install bat-extras manually. I also need to install shfmt so that bat-extras can be minified.
+- Fix somehow the vim message after e ++ff=unix.
--- dotfiles_ubuntu/essential_packages.sh
+++ dotfiles_ubuntu/essential_packages.sh
@@ -1,23 +1,35 @@
 #!/usr/bin/env bash
 
 echo "Installing essential packages and settings..."
+mkdir -p ~/tmp
 
+echo "Adding exa APT repository..."
 sudo add-apt-repository ppa:spvkgn/exa -y
 sudo apt-get update
 
-if ! command -v $1 &> /dev/null; then
-wget -O ~/tmp/bat_0.18.3_amd64.deb https://github.com/sharkdp/bat/releases/download/v0.18.3/bat_0.18.3_amd64.deb
-sudo dpkg -i ~/tmp/bat_0.18.3_amd64.deb
+if ! command -v bat &> /dev/null; then
+  echo "Installing bat..."
+  wget -O ~/tmp/bat_0.18.3_amd64.deb https://github.com/sharkdp/bat/releases/download/v0.18.3/bat_0.18.3_amd64.deb
+  sudo dpkg -i ~/tmp/bat_0.18.3_amd64.deb
 fi
+
+echo "Installing APT packages..."
 xargs -a Essentials.pckg sudo apt-get install -y
 
 chsh -s /bin/zsh
 
-curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+if ! command -v rustup &> /dev/null; then
+  echo "Installing Rust..."
+  curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+fi
 
-# https://github.com/eth-p/bat-extras
+if ! command -v batman &> /dev/null; then
+  echo "Installing bat extra modules..."
+  git clone "https://github.com/eth-p/bat-extras" ~/tmp/bat-extras
+  sudo ~/tmp/bat-extras/build.sh --install
+fi
 
-echo "Setting locale..."
+echo "Setting up locale..."
 sudo locale-gen pt_BR pt_BR.UTF-8
 sudo update-locale
 
