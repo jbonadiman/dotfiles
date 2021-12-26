a1f3042377bfef13ec0772a17b1e2cd19e659f90
João Vitor Bonadiman <j.bonadiman@live.com>
2021-12-26 19:06:13 -0300

    Adds wezterm config

---
+++ .wezterm.lua
@@ -0,0 +1,42 @@
+local wezterm = require 'wezterm'
+
+local wsl_distro = "kali-linux"
+
+return {
+	color_scheme = "OneHalfDark",
+	audible_bell = "Disabled",
+	default_cursor_style = "SteadyUnderline",
+	default_cwd = "~",
+	default_prog = {"wsl.exe", "--distribution", wsl_distro, "--cd", "~"},
+	exit_behavior = "Close",
+	swallow_mouse_click_on_pane_focus = true,
+	tab_bar_at_bottom = true,
+	window_close_confirmation = "NeverPrompt",
+	window_decorations = "RESIZE",
+	visual_bell = {
+		fade_in_duration_ms = 75,
+		fade_out_duration_ms = 75,
+		target = "CursorColor",
+	},
+	colors = {
+		visual_bell = "#202020",
+	},
+	font_size = 11.0,
+	font = wezterm.font({
+		"CozetteVector",
+	}),
+
+	add_wsl_distributions_to_launch_menu = false,
+	launch_menu = {
+		{
+			label = "Powershell",
+			args = {"powershell.exe"},
+			cwd = "~",
+		},
+		{
+			label = "zsh",
+			args = {"wsl.exe", "--distribution", wsl_distro},
+			cwd = "~",
+		},
+	}
+}
