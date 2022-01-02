4d865f4a85eb16ec21b3d9e67bf57ab59beddadd
João Vitor Bonadiman <joao@bonadiman.dev>
2022-01-02 14:54:37 -0300

    Changes wezterm cfg, adds firefox prefs

--- wezterm.lua
+++ wezterm.lua
@@ -50,4 +50,18 @@ if running_on_windows then
   }
 end
 
+wezterm.on("update-right-status", function(window, pane)
+  -- "Wed Mar 3 08:14"
+  local date = wezterm.strftime("%a %b %-d %H:%M ");
+
+  local bat = ""
+  for _, b in ipairs(wezterm.battery_info()) do
+    bat = "🔋 " .. string.format("%.0f%%", b.state_of_charge * 100)
+  end
+
+  window:set_right_status(wezterm.format({
+    {Text=bat .. "   "..date},
+  }));
+end)
+
 return config
